38d1817eaa201db8c64a64987dff3c79c7e5b917
Valentin Wettstein <valentin.wettstein@santismail.ch>
2021-03-16 14:03:35 +0100

    Commented Code

--- extension.js
+++ extension.js
@@ -1,66 +1,37 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-
-/**
- * @param {vscode.ExtensionContext} context
- */
-function activate(context) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Activated!');
-
-	const { exec } = require('child_process');
-	const DIR  = vscode.workspace.workspaceFolders[0].uri.fsPath;
-
-	//exec('git push', {cwd: DIR}, function(err, stdout, stderr) {
-
-		//console.log(stdout);
-
-	//});	
-
-}
-
-// this method is called when your extension is deactivated
+// This Function gets called when VS Code gets closed
 function deactivate() {
 
-	
-	const DIR  = vscode.workspace.workspaceFolders[0].uri.fsPath;
+	// Specify the Path of the Current Working Directory (CWD)
+	const CURRENT_WORKING_DIR  = vscode.workspace.workspaceFolders[0].uri.fsPath;
 
+	// Import Functionality to execute shell Commands
 	const { exec } = require('child_process');
-	const fs = require('fs');	
 
+	// Import File System Library
+	const fileSystem = require('fs');	
+
+	// Checks if a working folder has been opened in VS Code
 	if(vscode.workspace.workspaceFolders !== undefined) {
 
-		if(fs.existsSync(DIR + '/.gitignore')){
+		// Checks if a .gitignore file exists
+		if(fileSystem.existsSync(CURRENT_WORKING_DIR + '/.gitignore')){
 
-			console.log('.gitignore exists');
+			// Checks the status of the GIT Repo
+			exec('git status', {cwd: CURRENT_WORKING_DIR}, function(err, stdout, stderr) {
 
-			exec('git status', {cwd: DIR}, function(err, stdout, stderr) {
+				// Pushes to GIT Repo if repo is not up to Date
+				if(!stdout.includes('up to date')) exec('git push', {cwd: CURRENT_WORKING_DIR});
 
-				if(!stdout.includes('up to date')) continueExecuting(DIR, exec);
+			});		
 
-			});	
-			
 		} 
 
-	} 
-	
-}
+	} 	
 
-function continueExecuting(DIR, exec) {
-	exec('git push', {cwd: DIR}, function(err, stdout, stderr) {
-
-		console.log(stdout);
-
-	});	
 }
 
 module.exports = {
-	activate,
 	deactivate
 }
